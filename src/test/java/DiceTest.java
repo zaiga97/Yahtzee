5fd90bb729f8dd5f50004cce9736499184521510
Andrea Zasa <zaiga97@gmail.com>
2022-03-08 18:11:56 +0100

    Istantiation test with 5 faces fail

--- src/test/java/DiceTest.java
+++ src/test/java/DiceTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class DiceTest {
     @Test
@@ -8,5 +11,12 @@ public class DiceTest {
         Dice dice = new Dice(6);
 
         assertEquals(6, dice.getNumberFaces());
+    }
+
+    @Test
+    public void InstantiationFiveFacesTest(){
+        Dice dice = new Dice(5);
+
+        assertEquals(5, dice.getNumberFaces());
     }
 }
