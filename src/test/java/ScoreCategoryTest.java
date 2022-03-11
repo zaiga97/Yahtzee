6d366760c1840218d3f8ca23653ef446ef2e71c1
Andrea Zasa <zaiga97@gmail.com>
2022-03-11 16:09:26 +0100

    Completed the refactoring

--- src/test/java/ScoreCategoryTest.java
+++ src/test/java/ScoreCategoryTest.java
@@ -7,50 +7,18 @@ import scoring.category.SumCategory;
 import scoring.category.SumOfXCategory;
 
 import java.util.Arrays;
+import java.util.function.Function;
 
 
 public class ScoreCategoryTest {
+    public Function<int[], Integer> SUMF = ints -> Arrays.stream(ints).sum();
 
     @Test
     public void SumCategoryTest(){
         int[] diceValues = new int[] {1,2,3,4,5};
-        ScoreCategory sumCategory = new SumCategory();
-        assertEquals(15, sumCategory.calculateScore(diceValues));
-
-        assertEquals(false, sumCategory.isScored());
-        sumCategory.score(diceValues);
-        assertEquals(15, sumCategory.getScore());
-        assertEquals(true, sumCategory.isScored());
-    }
-
-    @Test
-    public void SumOf1CategoryTest(){
-        int[] diceValues = new int[] {1,2,3,4,5};
-        ScoreCategory sumOf1Category = new SumOfXCategory(1);
-        assertEquals(1, sumOf1Category.calculateScore(diceValues));
-        assertEquals(false, sumOf1Category.isScored());
-        sumOf1Category.score(diceValues);
-        assertEquals(1, sumOf1Category.getScore());
-        assertEquals(true, sumOf1Category.isScored());
-    }
-
-    @Test
-    public void SumOf4CategoryTest(){
-        int[] diceValues = new int[] {1,4,3,4,4};
-        ScoreCategory sumOf4Category = new SumOfXCategory(4);
-        assertEquals(4*3, sumOf4Category.calculateScore(diceValues));
-        assertEquals(false, sumOf4Category.isScored());
-        sumOf4Category.score(diceValues);
-        assertEquals(4*3, sumOf4Category.getScore());
-        assertEquals(true, sumOf4Category.isScored());
-    }
-
-    @Test
-    public void generalCategoryTest(){
-        int[] diceValues = new int[] {1,2,3,4,5};
         ScoreCategory generalCategory = new GeneralCategory(
                 "GeneralTest",
-                ints -> Arrays.stream(ints).sum()
+                SUMF
         );
         assertEquals(15, generalCategory.calculateScore(diceValues));
         assertEquals(false, generalCategory.isScored());
@@ -58,6 +26,5 @@ public class ScoreCategoryTest {
         assertEquals(15, generalCategory.getScore());
         assertEquals(true, generalCategory.isScored());
     }
-
 
 }
