cb8b3a2a7fbc9df00ac981266595377516d9ca98
Andrea Zasa <zaiga97@gmail.com>
2022-03-14 14:55:28 +0100

    capable of adding scores to score history

--- src/main/java/HighScore.java
+++ src/main/java/HighScore.java
@@ -1,8 +1,21 @@
+import java.util.*;
+
 public class HighScore {
-    public void add(String player1, int i) {
+    Map<String, List<Integer>> scoreHistory = new HashMap<>();
+
+    public void add(String name, int score) {
+
+        if(scoreHistory.containsKey(name)){
+            List<Integer> oldList = scoreHistory.get(name);
+            List<Integer> newList = new ArrayList<>(oldList);
+            newList.add(score);
+            scoreHistory.put(name, newList);
+        }
+
+        scoreHistory.putIfAbsent(name, Collections.singletonList(score));
     }
 
-    public int getScores(String player1) {
-        return 0;
+    public List<Integer> getScores(String name) {
+        return scoreHistory.get(name);
     }
 }
--- src/test/java/HighScoreTest.java
+++ src/test/java/HighScoreTest.java
@@ -1,12 +1,27 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HighScoreTest {
     @Test
     public void setScoreTest(){
         HighScore highScore = new HighScore();
-        highScore.add("Player1", 123);
-        assertEquals(123, highScore.getScores("Player1"));
+        highScore.add("Player1", 1);
+        List<Integer> expected = new ArrayList<>();
+        expected.add(1);
+        assertEquals(expected, highScore.getScores("Player1"));
+
+        highScore.add("Player1", 2);
+        expected.add(2);
+        assertEquals(expected, highScore.getScores("Player1"));
+
+        highScore.add("Player2", 1);
+        List<Integer> expected2 = new ArrayList<>();
+        expected2.add(1);
+        assertEquals(expected2, highScore.getScores("Player2"));
     }
 }
