6de40045ab3dbc5c611802d851ee07bea19a0c37
Andrea Zasa <zaiga97@gmail.com>
2022-03-14 15:40:24 +0100

    high score is now able to load from file

--- src/main/java/HighScore.java
+++ src/main/java/HighScore.java
@@ -1,9 +1,8 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.*;
 
 public class HighScore {
-    Map<String, List<Integer>> scoreHistory = new HashMap<>();
+    private Map<String, List<Integer>> scoreHistory = new HashMap<>();
 
     public void add(String name, int score) {
 
@@ -27,11 +26,38 @@ public class HighScore {
         try {
             Scanner scanner = new Scanner(new File(path));
             while (scanner.hasNextLine()){
-                add(scanner.next(), scanner.nextInt());
+                String line = scanner.nextLine();
+                if (!line.isEmpty()){
+                    String[] s = line.split(" ");
+                    add(s[0], Integer.parseInt(s[1]));
+                }
             }
             scanner.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    public void write(String path) {
+        try {
+            FileWriter fileWriter = new FileWriter(path);
+            for (String k: scoreHistory.keySet()) {
+                scoreHistory.get(k)
+                        .forEach(integer -> {
+                            try {
+                                fileWriter.write(k + " " + integer + "\n");
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        });
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Integer getRecord(String name) {
+        return scoreHistory.get(name).stream().max(Integer::compareTo).get();
     }
 }
--- src/test/java/HighScoreTest.java
+++ src/test/java/HighScoreTest.java
@@ -7,11 +7,10 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HighScoreTest {
+    HighScore highScore = new HighScore();
 
     @Test
     public void setScoreTest(){
-        HighScore highScore = new HighScore();
-
         highScore.add("Player1", 1);
         List<Integer> expected = new ArrayList<>();
         expected.add(1);
@@ -29,7 +28,6 @@ public class HighScoreTest {
 
     @Test
     public void ableToLoadFormFileTest(){
-        HighScore highScore = new HighScore();
         highScore.load("./src/test/resources/highScoreLoadTest");
 
         List<Integer> expected1 = new ArrayList<>();
@@ -43,5 +41,38 @@ public class HighScoreTest {
 
         assertEquals(expected1, highScore.getScores("Player1"));
         assertEquals(expected2, highScore.getScores("Player2"));
+    }
+
+    @Test
+    public void ableToWriteToFileTest(){
+        highScore.add("Player1", 1);
+        highScore.add("Player1", 1);
+        highScore.add("Player1", 2);
+
+        highScore.add("Player2", 3);
+        highScore.add("Player2", 1);
+
+        List<Integer> expected1 = new ArrayList<>();
+        expected1.add(1);
+        expected1.add(1);
+        expected1.add(2);
+
+        List<Integer> expected2 = new ArrayList<>();
+        expected2.add(3);
+        expected2.add(1);
+
+        highScore.write("./src/test/resources/highScoreWriteTest");
+        highScore = new HighScore();
+
+        highScore.load("./src/test/resources/highScoreWriteTest");
+        assertEquals(expected1, highScore.getScores("Player1"));
+        assertEquals(expected2, highScore.getScores("Player2"));
+    }
+
+    @Test
+    public void getRecordTest(){
+        highScore.load("./src/test/resources/highScoreLoadTest");
+        assertEquals(2, highScore.getRecord("Player1"));
+        assertEquals(3, highScore.getRecord("Player2"));
     }
 }
